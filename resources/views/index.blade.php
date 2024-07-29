f4e431240f1e3b07eeac23a9869005f335ee2808
Kenneth Mendoza <kenneth.b.mendoza@gmail.com>
2024-07-29 09:43:09 -0400

    update css and js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Branton - Contact Us</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-C6Gd1KfN.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-Df6Mo32p.css') }}">
     {{-- @vite(['resources/css/app.scss', 'resources/js/app.js'])  --}}
 
 </head>
